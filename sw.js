d833e95f191c5f8ceb3bd4c3e8ee2971823eb953
michaeljuniorbrice-debug <michaeljuniorbrice@gmail.com>
2026-09-06 21:55:28 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'edubridge-v2';
+const FILES = ['index.html','dashboard.html','pdf.html','video.html','manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
